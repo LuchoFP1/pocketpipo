99330d95d636977010194c0f3c28c1635c6d6736
Luciano Ferreyra <luchoferreyrapantanali@gmail.com>
2026-01-07 12:18:56 -0300

    chore: bootstrap spring boot project

---
+++ pocketpipo/src/main/java/com/pocketpipo/PocketpipoApplication.java
@@ -0,0 +1,13 @@
+package com.pocketpipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketpipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketpipoApplication.class, args);
+	}
+
+}
