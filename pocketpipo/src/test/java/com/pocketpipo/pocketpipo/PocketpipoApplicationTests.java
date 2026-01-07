e3fe0de0c4847315d23c181f2b13cd1e19bd8441
Luciano Ferreyra <luchoferreyrapantanali@gmail.com>
2026-01-07 19:33:37 -0300

    chore: added docker compose and application yml files

---
+++ pocketpipo/src/test/java/com/pocketpipo/pocketpipo/PocketpipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketpipo.pocketpipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketpipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
